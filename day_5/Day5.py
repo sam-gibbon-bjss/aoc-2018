ab12a4bed41c354e9db0cc3880dd67f2d2f5c9f2
Tetrinity <zephyrsg@hotmail.co.uk>
2018-12-13 14:50:48 +0000

    Day 5, part 1

---
+++ day_5/Day5.py
@@ -0,0 +1,47 @@
+from parse import compile
+
+
+def read_input():
+    with open('input.txt') as f:
+        return f.readline().strip()
+
+
+def will_react(unit1, unit2):
+    return unit1 != unit2 and unit1.upper() == unit2.upper()
+
+
+def react_iteration(polymer):
+    for i in range(0, len(polymer)-1):
+        # loop may skip some positions if we delete a character pair so can result in index out of range,
+        # but it's much less efficient to immediately break after one deletion
+        try:
+            if will_react(polymer[i], polymer[i + 1]):
+                polymer = polymer[:i] + polymer[i + 2:]
+        except IndexError:
+            break
+
+    return polymer
+
+
+def react(polymer):
+    previous_polymer = None
+    iterations = 0
+    while polymer != previous_polymer:
+        iterations += 1
+        previous_polymer = polymer
+        polymer = react_iteration(polymer)
+        # print("Reaction {}: {} -> {}".format(iterations, previous_polymer, polymer))
+        print("Reaction {}: {} -> {}".format(iterations, len(previous_polymer), len(polymer)))
+
+    return polymer
+
+
+def part1():
+    polymer = react(read_input())
+
+    print("Final polymer: {}".format(polymer))
+    print("Remaining units: {}".format(len(polymer)))
+
+
+if __name__ == '__main__':
+    part1()
